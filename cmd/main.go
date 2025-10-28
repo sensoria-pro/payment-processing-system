4eed9b009a8232b3a3b022610f13079eee23f5f9
sensoria-pro <sensoriaonline@gmail.com>
2025-10-28 12:38:42 +0300

    feat: add debug log for config

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,7 @@ func main() {
 		fallbackLogger.Error("Failed to load config", "ERROR", err)
 		os.Exit(1)
 	}
+	fallbackLogger.Info("✅ Config loaded", "db_url", cfg.Postgres.DSN, "app_port", cfg.Server.Port)
 	logger := observability.SetupLogger(cfg.App.Env)
 	logger.Info("The application is launched", "env", cfg.App.Env)
 
